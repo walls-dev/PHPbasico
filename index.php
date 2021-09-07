4887e62d7e6ad5ee591ae4a3559f64ffd8bf794a
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-07 19:38:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ switch($tipo){
 echo "<br/>";
 echo "<hr>";
 $numero = 0;
-while ($numero < 10){
-    echo "N: ". $numero;
+while ($numero <= 10){
+    echo "N: ". $numero."<br/>";
     $numero += 1;
 }
