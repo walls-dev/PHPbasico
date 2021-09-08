357601c30f63bada7a5ebebabc179d25d0a27d68
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-07 22:57:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -165,7 +165,8 @@ foreach($ingredientes as $chave => $valor){
 }
 echo "<br/>";
 echo "<hr>";
-for($i = 1; $i <= 10; $i++){
+for($i = "-"; $i == "-"; $i="-"){
+
 for ($i = 1; $i <= 10; $i++){
     echo "-"."<br/>";
 }
