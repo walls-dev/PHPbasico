81b3bfa064e8279baba082473d771420bc4855c4
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-09 22:20:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ $candidato = [
         'ing' => 2.0,
     ],
     ];
-    foreach($parametros as $chave => $valor){
+    foreach($candidato['parametros'] as $chave => $valor){
         echo "Candidato: ".$chave.": ".$valor."<br/>";
     }
 /*function mediafinal($candidato['parametros']['mat'],
