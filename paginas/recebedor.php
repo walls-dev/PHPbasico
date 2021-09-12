c88df2593ff42fef69f8e4d4a23d45d5096c6c3a
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 17:57:11 -0300

    Update recebedor.php

--- paginas/recebedor.php
+++ paginas/recebedor.php
@@ -10,7 +10,10 @@ COMO RECEBER AS INFORMAÇÕES DO FORMULÁRIO
 */
 $nome = filter_input(INPUT_POST, 'nome');
 $idade = filter_input(INPUT_POST, 'idade');
+if($nome && $idade){
 echo "NOME: ".$nome. "  |  "."IDADE: ".$idade;
-
+}else{
+    echo "Preencha todos os campos";
+}
 
 ?>
