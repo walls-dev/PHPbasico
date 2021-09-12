19959d7a51c32d3835ac9128f855eb36f9ec002d
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 11:57:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -346,3 +346,29 @@ ast();
 $data  = date('d/m/Y'); 
 $dia = date('N');
 echo $dia;
+switch ($dia){
+    case '0':
+        echo 'Domingo';
+        break;
+        case '1':
+            echo 'Segunda-feira';
+            break;
+            case '2':
+                echo 'Terça-feira';
+                break;
+                case '3':
+                    echo 'Quarta-feira';
+                    break;
+                    case '4':
+                        echo 'Quinta-feira';
+                        break;
+                        case '5':
+                            echo 'Sexta-feira';
+                            break;
+                            case '6':
+                                echo 'Sábado';
+                                break;
+                                case '7':
+                                    echo 'Domingo';
+                                    break; 
+}
