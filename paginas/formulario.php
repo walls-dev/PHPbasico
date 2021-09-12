3d413d5f5a61cb43cbf8baab235d89cb056c6090
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 16:45:46 -0300

    Update formulario.php

--- paginas/formulario.php
+++ paginas/formulario.php
@@ -2,3 +2,23 @@
 require_once('../template/header.php');
 ?>
 <h3>Formulário</h3>
+<form method= "GET" action="">
+
+<label for="">
+Nome:
+<br/>
+<input type="text" name="" id="">
+</label>
+<br/>
+<br/>
+
+<label for="">
+Idade:
+<br/>
+<input type="text" name="" id="">
+</label>
+<br/>
+<br/>
+
+<input type="submit" value="Enviar" >
+</form>
