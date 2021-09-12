1e8ef8d9c63b965a06009432b20d8415fee24372
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 11:13:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -341,5 +341,5 @@ echo date('d/m/Y', 0)."<br/>";//O ZERO É A QTD DE TIME QUE COLOCO OU SEJA IRÁ 
 echo date('d/m/Y', $time)."<br/>";//mostra a data atual no formato que quero
 echo date('d/m/Y', strtotime($data))."<br/>";;//forma reduzida*/
 
-echo "titulo";
+echo "titulo"."<br/>";
 ast();
