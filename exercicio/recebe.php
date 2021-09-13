2cb29267192f2d1b2b5363663ca0d3f2770869d5
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 23:27:43 -0300

    Update recebe.php

--- exercicio/recebe.php
+++ exercicio/recebe.php
@@ -6,10 +6,11 @@ session_start();
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 //3º VERIFICO SE EXISTE E REDIRECIONO PARA A INDEX
 if($nome){
-    header('Location: index.php');
-    exit;
+    
 }else{
     $_SESSION['aviso'] = 'Preencha os campos corretamente!';
+    header('Location: index.php');
+    exit;
 }
 //4º INICIO A SESSAO EM TODAS AS PAGINAS
 //5º VERIFICO SE A SESSAO EXISTE E SENÃO DIVULGO O AVISO
