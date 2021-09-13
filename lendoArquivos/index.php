69219895a16fd0b13244822dc7351431411fec11
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 19:36:49 -0300

    up

--- lendoArquivos/index.php
+++ lendoArquivos/index.php
@@ -1,4 +1,9 @@
 <?php
+/*PARA CONTAR O NÚMERO DE LINHAS DE UM TXT 
+1 - PEGO O ARQUIVO - file_get_contents('caminho do arquivo/ pode ser url externa')
+2 - CRIO UM EXPAÇO ENTRE AS LINHAS explode("\n", $variavel)
+3 - conto as linhas count($variavel)
+*/
 $pag = file_get_contents('pag.txt');
 $pag = explode("\n", $pag);
 echo "LINHAS:".count($pag);
