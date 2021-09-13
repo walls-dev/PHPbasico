b96b988462bafbf0029c852ea4500579c52dc81a
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 18:06:00 -0300

    up

--- paginas/recebedor.php
+++ paginas/recebedor.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 require_once('../template/header.php');
-/*  
-COMO RECEBER AS INFORMAÇÕES DO FORMULÁRIO
-obs: SÓ FAZER O REDIRECIONAMENTO ANTES DE EXIBIR OU ENVIAR QUALQUER COISA
-//SEMPRE DEPOIS DO HEADER LOCATION EXECUTAR O EXIT
-*/
+
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
