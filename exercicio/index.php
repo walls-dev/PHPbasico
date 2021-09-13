3d4e615c5e9c1aefcb662d9830fd49830b6d40ca
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 19:04:32 -0300

    Update index.php

--- exercicio/index.php
+++ exercicio/index.php
@@ -3,7 +3,6 @@ session_start();
 if($_SESSION['usuario']){
     echo "Olá, ".$_SESSION['usuario']."<br/>";
 }
-echo "<h3>PÁGINA INICIAL</h3>"."<br/>";
-
 ?>
+<h3>PÁGINA INICIAL</h3>
 <a href="">Sair</a>&nbsp;&nbsp;<a href="login.php">Login</a>
