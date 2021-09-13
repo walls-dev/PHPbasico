1b9959233d5f6469e8a6b3a7964688f0562d0e00
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 22:43:56 -0300

    up

---
+++ exercicio/login.php
@@ -0,0 +1,14 @@
+<?php
+echo "<h2>EXERCICIO</h2>";
+?>
+<form method= "POST" action="recebe.php">
+    <label>
+        Nome:
+        <input type="text" name="nome">
+        <br/>
+        <br/>
+        <input type="submit" value="Salvar">
+    </label>
+
+
+</form>
