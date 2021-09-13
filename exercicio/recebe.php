07ab7b048bc81936df53c093b597b70e43605294
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 23:31:00 -0300

    Update recebe.php

--- exercicio/recebe.php
+++ exercicio/recebe.php
@@ -7,6 +7,7 @@ $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 //3º VERIFICO SE EXISTE E REDIRECIONO PARA A INDEX
 if($nome){
     header('Location:index.php');
+    exit;
 }else{
     $_SESSION['aviso'] = 'Preencha os campos corretamente!';
     header('Location: login.php');
