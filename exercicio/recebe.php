a5f648d332bceabafa38ff60925c264a6df2e783
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 16:07:19 -0300

    up

--- exercicio/recebe.php
+++ exercicio/recebe.php
@@ -6,7 +6,6 @@ session_start();
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 //3º VERIFICO SE EXISTE E REDIRECIONO PARA A INDEX
 if($nome){
-    
     header('Location:index.php');
     exit;
 }else{
