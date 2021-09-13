8a089a45e752362bc34e4f25f337e76955650ca0
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 17:06:05 -0300

    up

--- exercicio/index.php
+++ exercicio/index.php
@@ -1,6 +1,6 @@
 <?php
+session_start();
 echo "<h3>PÁGINA INICIAL</h3>"."<br/>";
 echo "Olá, "."nomedoUsuario"."<br/>";
 ?>
-
 <a href="">Sair</a>&nbsp;&nbsp;<a href="login.php">Login</a>
--- exercicio/recebe.php
+++ exercicio/recebe.php
@@ -1,19 +1,16 @@
 <?php
-
+session_start();
 //1º RECEBO OS DADOS
 //$nome = filter_input(INPUT_POST, 'nome');
 //2º SANITIZO OS DADOS RECEBIDOS
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 //3º VERIFICO SE EXISTE E REDIRECIONO PARA A INDEX
 if($nome){
-    
-    
+    header('Location: index.php');
     exit;
 }else{
-    
-    
+    header('Location:login.php');
     exit;
-   
 }
 //4º INICIO A SESSAO EM TODAS AS PAGINAS
 //5º VERIFICO SE A SESSAO EXISTE E SENÃO DIVULGO O AVISO
