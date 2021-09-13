adb4f0413d3a7cb3597ebb20690739f9cf7f3e12
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 23:00:59 -0300

    Update recebe.php

--- exercicio/recebe.php
+++ exercicio/recebe.php
@@ -1,3 +1,11 @@
 <?php
 //1º RECEBO OS DADOS
-$nome = filter_input(INPUT_POST, 'nome');
+//$nome = filter_input(INPUT_POST, 'nome');
+//2º SANITIZO OS DADOS RECEBIDOS
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+//3º VERIFICO SE EXISTE E EXECUTO ALGO
+if($nome){
+    echo "NOME: ".$nome;
+}else{
+    echo "PREENCHA O CAMPO";
+}
