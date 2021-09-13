6f3f0ef7c375526bb279142796a65fb4563a4502
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-12 21:04:44 -0300

    Update recebedor.php

--- paginas/recebedor.php
+++ paginas/recebedor.php
@@ -4,15 +4,11 @@ require_once('../template/header.php');
 COMO RECEBER AS INFORMAÇÕES DO FORMULÁRIO
 obs: SÓ FAZER O REDIRECIONAMENTO ANTES DE EXIBIR OU ENVIAR QUALQUER COISA
 //SEMPRE DEPOIS DO HEADER LOCATION EXECUTAR O EXIT
-
-
-
-
 */
-$nome = filter_input(INPUT_POST, 'nome');
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$idade = filter_input(INPUT_POST, 'idade');
-if($nome && $email){
+$idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+if($nome && $email && $idade){
 echo "NOME : ".$nome."<br/>";
 echo "EMAIL: ".$email."<br/>";
 echo "IDADE: ".$idade;
@@ -20,5 +16,4 @@ echo "IDADE: ".$idade;
     header("Location: formulario.php");
     exit;
 }
-
 ?>
