1e38582b5d017f252792d8933c32241ac7778b98
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 23:10:58 -0300

    Update index.php

--- exPraticoTexto/index.php
+++ exPraticoTexto/index.php
@@ -10,9 +10,12 @@ echo "<h3>"."Lista de Nomes"."</h3>";
 ###############################################
 $nomes = file_get_contents('nomes.txt');
 $novoNome = filter_input(INPUT_POST, 'novoNome', FILTER_SANITIZE_SPECIAL_CHARS);
+$nomes = file_put_contents('nomes.txt', $novoNome);
 $nomes .="\n$novoNome";
 $nomes = explode("\n", $nomes);
 echo count($nomes);
+
+
 
 
 
