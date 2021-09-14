05cdeb7bdc9ccecda02730ff4ace3458aea85360
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 23:37:39 -0300

    Update index.php

--- exPraticoTexto/index.php
+++ exPraticoTexto/index.php
@@ -13,6 +13,7 @@ $novoNome = filter_input(INPUT_POST, 'novoNome', FILTER_SANITIZE_SPECIAL_CHARS);
 $nomes = file_get_contents('nomes.txt');//li
 $nomes.= "\n".$novoNome."\n";//adicionando novo nome
 $nomes = file_put_contents('nomes.txt', $novoNome);//salvando novo nome
+echo $nomes;
 
 
 
