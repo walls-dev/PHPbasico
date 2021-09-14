554943ff451bca05912e891c919fb22a644f9057
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 23:22:01 -0300

    up

--- exPraticoTexto/index.php
+++ exPraticoTexto/index.php
@@ -8,14 +8,13 @@ echo "<input type='submit' value='Adicionar'>";
 echo "</form>";
 echo "<h3>"."Lista de Nomes"."</h3>";
 ###############################################
-$nomes = file_get_contents('nomes.txt');
 $novoNome = filter_input(INPUT_POST, 'novoNome', FILTER_SANITIZE_SPECIAL_CHARS);
-
 $nomes = file_put_contents('nomes.txt', $novoNome);
+$nomes = file_get_contents('nomes.txt');
 ##$nomes .="\n$novoNome";
 ##$nomes = explode("\n", $nomes);
 ##echo count($nomes);
-echo $novoNome;
+echo $nomes;
 
 
 
