e45e1b56dc021946258c84e0521f806a616d8467
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-14 19:41:44 -0300

    up

--- exPraticoTexto/index.php
+++ exPraticoTexto/index.php
@@ -12,7 +12,7 @@ echo "<h3>"."Lista de Nomes"."</h3>";
 $novoNome = filter_input(INPUT_POST, 'novoNome', FILTER_SANITIZE_SPECIAL_CHARS);
 $nomes = file_get_contents('nomes.txt');
 $nomes .= "\n$novoNome";//adicionando novo nome
-$nomes = file_put_contents('nomes.txt', $novoNome);//salvando novo nome
+##$nomes = file_put_contents('nomes.txt', $novoNome);//salvando novo nome
 $nomes = explode("\n", $nomes);
 foreach($nomes as $nome){
     echo "<li>". $nome ."</li>";
