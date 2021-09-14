a52c532b230de5751f5ae23fc0587062a9e572cc
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 21:43:39 -0300

    up

--- escrevendoemArquivos/index.php
+++ escrevendoemArquivos/index.php
@@ -2,3 +2,5 @@
 $texto = 'Esse é o texto a ser inserido no arquivo externo';
 file_put_contents('nome.txt', $texto);
 echo 'Arquivo criado com sucesso!';
+
+/* O file_put_contents recebe dois parametros: o nome do arquivo externo e a variável do texto a ser inserido. OBS: CASO O ARQUIVO EXTERNO NÃO EXISTA, ELE CRIA AUTOMATICAMENTE E INSERE O TEXTO */
