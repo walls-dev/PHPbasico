a183bc3c3c85b95f0d83bb888a6d32a69643257b
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-14 20:48:17 -0300

    Update index.php

--- exPraticoTexto/index.php
+++ exPraticoTexto/index.php
@@ -1,5 +1,4 @@
 <?php
-
 echo "<form action='index.php' method='POST'>";
 echo "<label>";
 echo "Novo Nome:"."<br/>";
@@ -7,24 +6,18 @@ echo "<input type='text' name='novoNome'>";
 echo "</label>";
 echo "<input type='submit' value='Adicionar'>";
 echo "</form>";
+###############################################################################
 echo "<h3>"."Lista de Nomes"."</h3>";
-###############################################
 $novoNome = filter_input(INPUT_POST, 'novoNome', FILTER_SANITIZE_SPECIAL_CHARS);
 $nomes = file_get_contents('nomes.txt');
 $nomes .= "\n$novoNome";//adicionando novo nome
 $nomes = file_put_contents('nomes.txt', $nomes);//salvando os nomes
 $nomes = explode("\n", $nomes);
 foreach($nomes as $nome){
-    echo "<li>". $nome ."</li>";
+    echo "<li>". $nomes ."</li>";
 }
 
 
-/*echo "<pre>";
-print_r($nomes);
-echo "</pre>";
-/*$pag = file_get_contents('pag.txt');
-$pag = explode("\n", $pag);
-echo "LINHAS:".count($pag);*/
 
 
 
