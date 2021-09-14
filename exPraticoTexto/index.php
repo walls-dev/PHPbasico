ed1e6488da1c80a794fb0e065826f630d2f29f03
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-13 23:05:05 -0300

    Update index.php

--- exPraticoTexto/index.php
+++ exPraticoTexto/index.php
@@ -1,13 +1,16 @@
-<form action="index.php" method="POST">
-<label>
-Novo Nome:<br/>
-<input type="text" name="novoNome">
-</label>
-<input type="submit" value="Adicionar">
-</form>
-<h3>Lista de Nomes</h3>
 <?php
+echo "<form action='index.php' method='POST'>";
+echo "<label>";
+echo "Novo Nome:"."<br/>";
+echo "<input type='text' name=''>";
+echo "</label>";
+echo "<input type='submit' value='Adicionar'>";
+echo "</form>";
+echo "<h3>"."Lista de Nomes"."</h3>";
+
 $nomes = file_get_contents('nomes.txt');
+$novoNome = '';
+$nomes .="\n$novoNome";
 $nomes = explode("\n", $nomes);
 echo count($nomes);
-?>
+
