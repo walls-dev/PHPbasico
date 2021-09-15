93587c90c929dd2c9ad61e30c7ecbd915116cfc1
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-14 22:24:03 -0300

    Update index.php

--- movendoArquivos/index.php
+++ movendoArquivos/index.php
@@ -3,5 +3,6 @@
 //echo "Arquivo renomeado com sucesso";
 //PARA MOVER É O MESMO COMANDO
 rename('novoNome.txt', '../paginas/novoNome.txt');
+//SE FOR COPIAR O ARQUIVO É copy('novoNome.txt', 'url de destino');
 
 echo "Arquivo movido para dentro da pasta";
