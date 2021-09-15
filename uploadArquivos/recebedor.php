2e8b2dba50c6b4e7534c05aaa736f73a016aebb9
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-15 17:23:12 -0300

    ...

--- uploadArquivos/recebedor.php
+++ uploadArquivos/recebedor.php
@@ -19,6 +19,14 @@ print_r($_FILES);//VARIÁVEL GLOBAL
 //o 1º é onde está na pasta temporaria [ arquivo é o name do input][tmp_name]
 //o 2º é onde que mover o arquivo nome da pasta + o nome do arquivo
 //
-//$permitidos = [''];
-$nome = $_FILES['arquivo']['name'];//usando o nome original
-move_uploaded_file($_FILES['arquivo']['tmp_name'],'Arquivos/'.$nome);
+//$permitidos = ['text/plain.txt', 'application.pdf'];
+$permitidos = ['text/plain.txt'];
+if(in_array($_FILES['arquivo']['type'], $permitidos)){
+    //$nome = $_FILES['arquivo']['name'];//usando o nome original
+    $nome = md5(time().rand(0, 1000)).'txt';//para não sobrescrever - gerar outro nome
+    move_uploaded_file($_FILES['arquivo']['tmp_name'],'Arquivos/'.$nome);
+    echo "Arquivo Salvo com sucesso!";
+}else{
+    echo "Arquivo não permitido!";
+}
+
