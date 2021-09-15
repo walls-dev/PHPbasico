3eee2870c8a764fa2a7601ba428dfa6321239c0c
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-15 20:32:30 -0300

    Update index.php

--- manipulaArray/index.php
+++ manipulaArray/index.php
@@ -1,6 +1,6 @@
 <?php
 //$array = ['1', '2', '3'];
-$array = range(900001, 9000050);//range tem 2 parametros
+$array = range(900001, 900050);//range tem 2 parametros
 // o 1º é o que vai começar e o 2º é o que vai terminar
 foreach($array as $item){
     echo $item."<br/>";
