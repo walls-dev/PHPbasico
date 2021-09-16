5b868ec9c04ae648a3f11c474f9d177048d8997b
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-16 20:15:17 -0300

    Update index.php

--- manipulaArray/index.php
+++ manipulaArray/index.php
@@ -67,18 +67,19 @@ $array = [
         'profissao' => 'SIM',
         'TESTE' => 'OK',
 ];
-
+$chaves = array_keys($array);
+$valores = array_values($array);
 
 ?>
 
 <table border="1">
     <tr>
-        <?php foreach($array as $chave => $valor): ?>
+        <?php foreach($chaves as $chave): ?>
                 <th><?php echo $chave;?></th>
         <?php endforeach; ?>
     </tr>
     <tr>
-        <?php foreach($array as $chave => $valor): ?>
+        <?php foreach($valores as $valor): ?>
                 <td><?php echo $valor;?></td>
         <?php endforeach; ?>
     </tr>
