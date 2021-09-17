6756b1cb3fd817a28c0321da07edda375b006b9c
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-16 21:38:49 -0300

    Update index.php

--- arraySlice/index.php
+++ arraySlice/index.php
@@ -1,4 +1,4 @@
 <?php
 $array = ['a', 'b', 'c', 'd', 'e', 'f'];
-$retorno = array_slice($array, 0, 2);
+$retorno = array_slice($array, 0, 2);//primeoro parametro é de onde começa e 2º qtd de itens
 print_r($retorno);
