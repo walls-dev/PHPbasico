808994deb15e6a7313585caaf4f51482af25c597
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-16 21:53:55 -0300

    Update index.php

--- arraySlice/index.php
+++ arraySlice/index.php
@@ -5,5 +5,6 @@ $retorno = array_slice($array, 0, 3);//primeoro parametro é de onde começa e 2
 print_r($retorno);
 echo "<br/>";
 
-$alterado = array_splice($array, 2, 1); //remove o array posicao 2
+//$alterado = array_splice($array, 2, 1); //remove o array posicao 2
+$alterado = array_splice($array, 0, 1, 'g');//remove o item 'a' e acrescenta o 'g'.
 print_r($array);
