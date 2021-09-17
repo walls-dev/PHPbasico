8454984d1bf97738b3c4014090ed7f5e7c77eb82
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-17 19:31:38 -0300

    Update index.php

--- oo/index.php
+++ oo/index.php
@@ -12,4 +12,5 @@ $post1->likes = 3;
 $post2 = new Post();
 $post2->likes = 10;
 ###############################################
-echo "POST 1: ".$post1->likes;
+echo "POST 1: ".$post1->likes."<br/>";
+echo "POST 2: ".$post2->likes;
