a48a86538b9882f08a8e4f3649bae93876023141
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-17 23:27:26 -0300

    Update index.php

--- oo/metodo_estatico/index.php
+++ oo/metodo_estatico/index.php
@@ -1,15 +1,15 @@
 <?php
 class Matematica {
 
-
+    public static string $nome = 'Walls';
     public static function somar($x, $y){
         return $x + $y;
     }
 
 }
 
-echo Matematica::somar(50, 50);
-
+echo Matematica::somar(50, 50)."<br/>";
+echo Matematica::$nome;
 //a vantagem da funçao estática é que não precisa instanciar a classe ou seja criar um
 //objeto para que seja aproveitada aquela função
 //OS :: SIGNIFICA ACESSE A CLASSE OU A PROPRIEDADE TAL
