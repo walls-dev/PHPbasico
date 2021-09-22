4cc5a5a292492062f10e8f73b7dd2e2e64f92bc7
Walls P Silva <70984491+walls-dev@users.noreply.github.com>
2021-09-22 11:14:04 -0300

    ,,,

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8276c7384ba6a9eeca28b0d29475cc6503d5cfa8',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8276c7384ba6a9eeca28b0d29475cc6503d5cfa8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
